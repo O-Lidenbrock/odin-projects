9599bad0898175c9b765de970196e1a59ceb3169
butler <113191739+SchrodingersMouser@users.noreply.github.com>
2025-01-05 09:03:44 -0600

    started a place for all my TOP JS

---
+++ javascript.js
@@ -0,0 +1,28 @@
+let name = "John";
+let surname = "Doe";
+
+const pi = 3.14
+
+console.log(23+97);
+
+//js only has one type of number (64bit floating point)
+
+/* integers are accurate to 15 digits
+max number of decimals is 17
+
+floating point arithmetic is not always 100 percent accurate
+
+numbers are added, strings are concatenated
+
+== is equal to
+=== is equal value and equal type
+
+you can increment after or before
+*/
+
+let counter = 1;
+let a = counter++; // a is equal to 1 and counter equals 2. if you want a = 2, do ++counter
+
+//unary plus converts strings to numbers
+let apples = "2"
+alert(+apples + 4) // returns 6
